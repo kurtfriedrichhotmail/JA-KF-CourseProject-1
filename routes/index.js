1033aba5b265cd92d8160ffc71abeb0aae325d5c
bcuser <jabuan0910@yahoo.com>
2024-03-07 17:15:22 -0800

    updated and added filemanager

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var cors = require('cors');
 
+var app = express();
+
+
+// start by creating data so we don't have to type it in each time
 let ServerRecipeArray = [];
 
+// define a constructor to create recipe objects
 let RecipeObject = function (pName, pCuisine, pDifficulty, pURL) {
     this.ID = Math.random().toString(16).slice(2, 10); // Generates a random ID
     this.name = pName;
@@ -21,8 +28,10 @@ router.delete('/DeleteRecipe/:ID', (req, res) => {
     return res.status(500).json({
         status: "error - no such ID"
     });
+
   } else {  // if did find the recipe
     ServerRecipeArray.splice(pointer, 1);  // remove 1 element at index
+    fileManager.write();
     res.send('Recipe with ID: ' + delID + ' deleted!');
   }
 });
@@ -31,10 +40,42 @@ function GetObjectPointer(localID) {
     return ServerRecipeArray.findIndex(recipe => recipe.ID === localID);
 }
 
-// Preload some recipes
+// var fs = require("fs"); // relocate to line 3
+
+let fileManager = {
+  read: function() {
+    var rawdata = fs.readFileSync('objectdata.json');
+    let goodData = JSON.parse(rawdata);
+    ServerRecipeArray = goodData;
+  },
+
+  write: function() {
+    let data = JSON.stringify(ServerRecipeArray);
+    fs.writeFileSync('objectdata.json', data);
+  },
+
+  validData: function() {
+    var rawdata = fs.readFileSync('objectdata.json');
+    console.log(rawdata.length);
+    if(rawdata.length < 1) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+};
+
+if(!fileManager.validData()) {
 ServerRecipeArray.push(new RecipeObject("Chicken & Spinach Skillet Pasta", "American", "Medium", "https://www.eatingwell.com/recipe/267768/chicken-spinach-skillet-pasta-with-lemon-parmesan/"));
 ServerRecipeArray.push(new RecipeObject("Spaghetti & Spinach with Sun-Dried Tomato", "Italian", "Easy", "https://www.eatingwell.com/recipe/7919563/spaghetti-spinach-with-sun-dried-tomato-cream-sauce/"));
 ServerRecipeArray.push(new RecipeObject("One-Pot Garlicky Shrimp & Broccoli ", "Chinese", "Medium", "https://www.eatingwell.com/recipe/7919492/one-pot-garlicky-shrimp-broccoli/"));
+fileManager.write();
+}
+
+else {
+  fileManager.read(); // do have prior recipes so load up the array
+  }
 
 console.log(ServerRecipeArray);
 
@@ -45,6 +86,7 @@ router.get('/', function(req, res, next) {
 
 /* GET all Recipe data */
 router.get('/getAllRecipes', function(req, res) {
+  fileManager.read()
   res.status(200).json(ServerRecipeArray);
 })
 
@@ -52,6 +94,7 @@ router.get('/getAllRecipes', function(req, res) {
 router.post('/AddRecipe', function(req, res) {
   const newRecipe = req.body;
   ServerRecipeArray.push(newRecipe);
+  fileManager.write();
   res.status(200).json(newRecipe);
 });
 
